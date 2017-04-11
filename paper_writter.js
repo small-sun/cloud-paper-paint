fc6ba00e2f9e061dadab3e7e3f6334dcb34fea7b
snovey <isnovey@gmail.com>
2017-04-11 13:18:35 +0800

    重构了 paper_writter 对象

--- paper_writter.js
+++ paper_writter.js
@@ -20,27 +20,25 @@
       let canvas = obj.el.querySelector('canvas');
       let type = document.getElementById('type'); //笔型
       let size = document.getElementById('size'); //笔的粗细
+      this.type = type.value;
+      this.size = size.value;
+      this.init(canvas, type, size);
+    }
+
+    init (canvas, type, size) {
       let context = canvas.getContext('2d');
       let _self = this;
       let current, last; //记录上一次与本次事件的坐标
-      this.type = type.value;
-      this.size = size.value;
-      type.addEventListener('change', function(e) {
-        this.type = e.target.value;
-      });
-      size.addEventListener('change', function() {
-        this.size = e.target.value;
-      });
-      canvas.addEventListener('touchstart', function (e) {
+      let start = function (e) {
         e.preventDefault();
-        context.lineWidth = this.lineWidth;
-        context.color = this.color;
-        last = _self.collectInfo(e);
+        context.lineWidth = _self.lineWidth;
+        context.color = _self.color;
+        last = _self.getInfo(e);
         message.push(last);
-      });
-      canvas.addEventListener('touchmove', function (e) {
+      };
+      let move = function (e) {
         e.preventDefault();
-        current = _self.collectInfo(e);
+        current = _self.getInfo(e);
         message.push(current);
         context.beginPath();
         context.moveTo(last.x, last.y);
@@ -48,27 +46,57 @@
         context.stroke();
         context.closePath();
         last = current;
-      });
-      canvas.addEventListener('touchend', function (e) {
+      };
+      let end = function (e) {
         e.preventDefault();
+      };
+      type.addEventListener('change', e => {
+        this.type = e.target.value;
       });
+      size.addEventListener('change', e => {
+        this.size = e.target.value;
+      });
+      canvas.addEventListener('touchstart', function (e) {
+        start(e);
+        canvas.addEventListener('touchmove',move , {passive: false});
+        canvas.addEventListener('touchend', function _self() {
+          canvas.removeEventListener('touchmove', move, {passive: false});
+          canvas.removeEventListener('touchend', _self, {passive: false});
+        }, {passive: false});
+      }, {passive: false});
+      canvas.addEventListener('mousedown', function (e) {
+        start(e);
+        canvas.addEventListener('mousemove', move, {passive: false});
+        canvas.addEventListener('mouseup', function _self() {
+          canvas.removeEventListener('mousemove', move, {passive: false});
+          canvas.removeEventListener('mouseup', _self, {passive: false});
+        }, {passive: false});
+      }, {passive: false});
     }
 
-    collectInfo (e) {
-      return {
+    getInfo (e) {
+      let x, y;
+      if (e.type.substring(0, 5) === 'touch') {
+        x = e.touches[0].clientX - e.currentTarget.getBoundingClientRect().left;
+        y = e.touches[0].clientY - e.currentTarget.getBoundingClientRect().top;
+      } else {
+        x = e.clientX - e.currentTarget.getBoundingClientRect().left;
+        y = e.clientY - e.currentTarget.getBoundingClientRect().top;
+      }
+      return new Position({
         width: this.width,
         height: this.height,
         timeStamp: e.timeStamp,
         type: e.type,
-        x: e.touches[0].clientX - e.currentTarget.getBoundingClientRect().left,
-        y: e.touches[0].clientY - e.currentTarget.getBoundingClientRect().top,
+        x: x,
+        y: y,
         pen: {
           type: this.type,
           size: this.size,
           color: 'black',
           opacity: 1
         }
-      };
+      });
     }
   };
 })();
