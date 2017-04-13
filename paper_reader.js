0d6626372e3ea5a79447adc51fcebcaf5bfa94c5
snovey <isnovey@gmail.com>
2017-04-13 15:22:40 +0800

    大幅重构

--- paper_reader.js
+++ paper_reader.js
@@ -15,28 +15,99 @@
           _self.factor = Math.min(_self.width/last.width, _self.height/last.height);
           switch(last.pen.type) {
             case 1:
+              // console.log(last.type);
               context.lineWidth = last.pen.size*_self.factor;
               context.strokeStyle = last.pen.color;
               while(message.length) {
                 current = message.shift();
-                context.beginPath();
-                context.moveTo(last.x*_self.factor, last.y*_self.factor);
-                context.lineTo(current.x*_self.factor, current.y*_self.factor);
-                context.stroke();
-                context.closePath();
+                _self.drawLine (context, last, current);
                 last = current;
+                // console.log(current.type);
               }
               break;
             case 2:
               context.clearRect(last.x*_self.factor, last.y*_self.factor, last.pen.size*3*_self.factor, last.pen.size*3*_self.factor);
               while(message.length) {
                 current = message.shift();
+                // console.log(current.type);
                 context.clearRect(current.x*_self.factor, current.y*_self.factor, last.pen.size*3*_self.factor, last.pen.size*3*_self.factor);
               }
               break;
+            case 4:
+              // console.log(last.type.substring(5));
+              while(message.length) {
+                current = message.shift();
+                // console.log(current.type.substring(5));
+              }
           }
         }
       }, 500);
+    }
+
+    fork (e, move, end) {
+      let duplicate, layer;
+      duplicate = document.createElement('canvas');
+      duplicate.width = this.width;
+      duplicate.height = this.height;
+      duplicate.style.position = 'absolute';
+      duplicate.style.left = e.currentTarget.getBoundingClientRect().left+'px';
+      duplicate.style.top = e.currentTarget.getBoundingClientRect().top+'px';
+      duplicate.style['z-index'] = 1;
+      layer = duplicate.getContext('2d');
+      document.body.appendChild(duplicate);
+      duplicate.addEventListener('touchmove', move, {passive: false});
+      document.addEventListener('touchend', function _self() {
+        duplicate.removeEventListener('touchmove', move, {passive: false});
+        duplicate.removeEventListener('touchend', _self, {passive: false});
+        end(e);
+      }, {passive: false});
+      duplicate.addEventListener('mousemove', move, {passive: false});
+      document.addEventListener('mouseup', function _self() {
+        duplicate.removeEventListener('mousemove', move, {passive: false});
+        duplicate.removeEventListener('mouseup', _self, {passive: false});
+        end(e);
+      }, {passive: false});
+      return [duplicate, layer];
+    }
+
+    bindCanvas (canvas, start, move, end) {
+      canvas.addEventListener('touchstart', e => {
+        start(e);
+        canvas.addEventListener('touchmove', move, {passive: false});
+        document.addEventListener('touchend', function _self() {
+          canvas.removeEventListener('touchmove', move, {passive: false});
+          canvas.removeEventListener('touchend', _self, {passive: false});
+        }, {passive: false});
+      }, {passive: false});
+      canvas.addEventListener('mousedown', e => {
+        start(e);
+        canvas.addEventListener('mousemove', move, {passive: false});
+        document.addEventListener('mouseup', function _self() {
+          canvas.removeEventListener('mousemove', move, {passive: false});
+          canvas.removeEventListener('mouseup', _self, {passive: false});
+        }, {passive: false});
+      }, {passive: false});
+    }
+
+    drawLine (context, first, last) {
+      context.beginPath();
+      context.moveTo(first.x, first.y);
+      context.lineTo(last.x, last.y);
+      context.stroke();
+      context.closePath();
+    }
+
+    drawEllipse(context, x, y, a, b) {
+      context.save();
+      var r = (a > b) ? a : b;
+      var ratioX = a / r;
+      var ratioY = b / r;
+      context.scale(ratioX, ratioY);
+      context.beginPath();
+      context.arc(x / ratioX, y / ratioY, r, 0, 2 * Math.PI, false);
+      context.closePath();
+      context.restore();
+      context.stroke();
     }
   };
 })();
--- paper_writter.js
+++ paper_writter.js
@@ -8,7 +8,7 @@
       node.width = this.width;
       node.height = this.height;
       obj.el.appendChild(node);
-      const canvas = obj.el.querySelector('canvas');
+      const canvas = node;
       const type = obj.type;
       const size = obj.size;
       const color = obj.color;
@@ -20,54 +20,9 @@
 
     init (canvas, type, size, color) {
       const context = canvas.getContext('2d');
-      const _self = this;
       let current, last; //记录上一次与本次事件的坐标
-      let start = function (e) {
-        e.preventDefault();
-        last = _self.getInfo(e);
-        switch (_self.type) {
-          case 1:
-            context.lineWidth = _self.size;
-            context.strokeStyle = _self.color;
-            break;
-          case 2:
-            // 这里需要 CSS 光标样式
-            break;
-        }
-        message.push(last);
-      };
-      let move = function (e) {
-        e.preventDefault();
-        current = _self.getInfo(e);
-        message.push(current);
-        switch (_self.type) {
-          case 1:
-            context.beginPath();
-            context.moveTo(last.x, last.y);
-            context.lineTo(current.x, current.y);
-            context.stroke();
-            context.closePath();
-            break;
-          case 2:
-            context.clearRect(current.x, current.y, _self.type*10, _self.type*10);
-            break;
-        }
-        last = current;
-      };
-      let end = function (e) {
-        e.preventDefault();
-      };
-
-      let writeText = function (e) {
-        current = _self.getInfo(e);
-        let text = document.createElement('div');
-        text.className = 'text';
-        text.style.left = e.clientX+'px';
-        text.style.top = e.clientY+'px';
-        console.log(canvas);
-        document.body.appendChild(text);
-        message.push(current);
-      };
+      let corner; //记录矩形起点
+      let duplicate, layer; //第二个图层
 
       type.addEventListener('change', e => {
         this.type = parseInt(e.target.value);
@@ -84,31 +39,161 @@
         this.color = e.target.value;
       });
 
-      this.bindEvent(canvas, start, move, end);
+      let start = e => {
+        e.preventDefault();
+        last = this.getInfo(e);
+        switch (this.type) {
+          case 1: //画笔
+            context.lineWidth = this.size;
+            context.strokeStyle = this.color;
+            break;
+          case 2: //橡皮
+            // 这里需要设置 CSS 光标样式
+            break;
+          case 4: //矩形
+          case 5: //椭圆
+            corner = last;
+            [duplicate, layer] = this.fork(e, move, end);
+            break;
+        }
+        message.push(last);
+      }
+
+      let move = e => {
+        e.preventDefault();
+        current = this.getInfo(e);
+        message.push(current);
+        switch (this.type) {
+          case 1:
+            this.drawLine(context, last, current);
+            break;
+          case 2:
+            context.clearRect(current.x, current.y, this.type*10, this.type*10);
+            break;
+          case 4:
+            layer.clearRect(0, 0, duplicate.width, duplicate.height);
+            layer.strokeRect(corner.x, corner.y, current.x-corner.x, current.y-corner.y);
+            break;
+          case 5:
+            layer.clearRect(0, 0, duplicate.width, duplicate.height);
+            this.drawEllipse(layer, corner.x, corner.y, current.x-corner.x, current.y-corner.y);
+            break;
+        }
+        last = current;
+      }
+
+      let end = e => {
+        console.log(e.type);
+        current = this.getInfo(e, last);
+        e.preventDefault();
+        switch(this.type) {
+          case 4:
+            context.strokeRect(corner.x, corner.y, current.x-corner.x, current.y-corner.y);
+            break;
+          case 5:
+            this.drawEllipse(context, corner.x, corner.y, current.x-corner.x, current.y-corner.y);
+            break;
+        }
+        message.push(current);
+      }
+
+      let writeText = e => {
+        current = this.getInfo(e);
+        let text = document.createElement('div');
+        text.className = 'text';
+        text.style.left = e.clientX+'px';
+        text.style.top = e.clientY+'px';
+        document.body.appendChild(text);
+        // message.push(current);
+      }
+
+      this.bindCanvas(canvas, start, move, end);
     }
 
-    bindEvent (canvas, start, move, end) {
-      canvas.addEventListener('touchstart', function (e) {
+    fork (e, move, end) {
+      let duplicate, layer;
+      duplicate = document.createElement('canvas');
+      duplicate.width = this.width;
+      duplicate.height = this.height;
+      duplicate.style.position = 'absolute';
+      duplicate.style.left = e.currentTarget.getBoundingClientRect().left+'px';
+      duplicate.style.top = e.currentTarget.getBoundingClientRect().top+'px';
+      duplicate.style['z-index'] = 1;
+      layer = duplicate.getContext('2d');
+      document.body.appendChild(duplicate);
+
+      duplicate.addEventListener('touchmove', move, {passive: false});
+      document.addEventListener('touchend', function _self(e) {
+        end(e);
+        duplicate.removeEventListener('touchmove', move, {passive: false});
+        document.removeEventListener('touchend', _self, {passive: false});
+        document.body.removeChild(duplicate);
+      }, {passive: false});
+
+      duplicate.addEventListener('mousemove', move, {passive: false});
+      document.addEventListener('mouseup', function _self(e) {
+        console.log('hello');
+        end(e);
+        duplicate.removeEventListener('mousemove', move, {passive: false});
+        document.removeEventListener('mouseup', _self, {passive: false});
+        document.body.removeChild(duplicate);
+      }, {passive: false});
+
+      return [duplicate, layer];
+    }
+
+    bindCanvas (canvas, start, move, end) {
+      canvas.addEventListener('touchstart', e => {
         start(e);
-        canvas.addEventListener('touchmove',move , {passive: false});
-        document.addEventListener('touchend', function _self() {
+        canvas.addEventListener('touchmove', move, {passive: false});
+        document.addEventListener('touchend', function _self(e) {
+          end(e);
           canvas.removeEventListener('touchmove', move, {passive: false});
-          canvas.removeEventListener('touchend', _self, {passive: false});
+          document.removeEventListener('touchend', _self, {passive: false});
         }, {passive: false});
       }, {passive: false});
-      canvas.addEventListener('mousedown', function (e) {
+      canvas.addEventListener('mousedown', e => {
         start(e);
         canvas.addEventListener('mousemove', move, {passive: false});
-        document.addEventListener('mouseup', function _self() {
+        document.addEventListener('mouseup', function _self(e) {
+          console.log('canvas');
+          end(e);
           canvas.removeEventListener('mousemove', move, {passive: false});
-          canvas.removeEventListener('mouseup', _self, {passive: false});
+          document.removeEventListener('mouseup', _self, {passive: false});
         }, {passive: false});
       }, {passive: false});
     }
 
-    getInfo (e) {
+    drawLine (context, first, last) {
+      context.beginPath();
+      context.moveTo(first.x, first.y);
+      context.lineTo(last.x, last.y);
+      context.stroke();
+      context.closePath();
+    }
+
+    drawEllipse(context, x, y, a, b) {
+      context.save();
+      var r = (a > b) ? a : b;
+      var ratioX = a / r;
+      var ratioY = b / r;
+      context.scale(ratioX, ratioY);
+      context.beginPath();
+      context.arc(x / ratioX, y / ratioY, r, 0, 2 * Math.PI, false);
+      context.closePath();
+      context.restore();
+      context.stroke();
+    }
+
+    getInfo (e, last) {
       let x, y;
-      if (e.type.substring(0, 5) === 'touch') {
+      if (e.type === 'touchend') { //touchend 和 mouseup 这两个事件比较奇葩，需要特殊处理
+        x = last.x;
+        y = last.y;
+      } else if (e.type === 'mouseup') { //mouseup 有 x, y 坐标，不过先偷懒一下
+        x = last.x;
+        y = last.y;
+      } else if (e.type.substring(0, 5) === 'touch') {
         x = e.touches[0].clientX - e.currentTarget.getBoundingClientRect().left;
         y = e.touches[0].clientY - e.currentTarget.getBoundingClientRect().top;
       } else {
